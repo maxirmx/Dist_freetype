4391492e24728ae9d2c8aedcde581f9da70a4077
maxirmx <maxim@samsonov.net>
2019-11-08 18:26:33 +0300

    modified:   .appveyor.yml 	deleted:    local/load-freetype.ps1

--- local/load-freetype.ps1
+++
@@ -1,8 +0,0 @@
-Invoke-WebRequest -Uri http://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-2-10-1.tar.gz -OutFile freetype2-VER-2-10-1.tar.gz
-& "C:\Program Files\7-Zip\7z" -aoa x freetype2-VER-2-10-1.tar.gz 
-& "C:\Program Files\7-Zip\7z" -aoa x freetype2-VER-2-10-1.tar 
-del freetype2-VER-2-10-1.tar.gz
-del freetype2-VER-2-10-1.tar
-del pax_global_header
-ren freetype2-VER-2-10-1 freetype
-
