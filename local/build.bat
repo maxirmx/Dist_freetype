f1774cd6f8fcc2d7d861cfe2c18e098cb014486f
maxirmx <maxim@samsonov.net>
2019-11-08 18:45:57 +0300

    modified:   .appveyor.yml 	modified:   local/build.bat 	renamed:    run_cmake.cmd -> local/run_cmake.cmd

--- local/build.bat
+++ local/build.bat
@@ -1,3 +1,3 @@
-"C:\Program Files\CMake\bin\cmake" -E make_directory build
-"C:\Program Files\CMake\bin\cmake" -E chdir build cmake -D CMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -G "NMake Makefiles" ..
-"C:\Program Files\CMake\bin\cmake" --build build
+powershell -executionpolicy bypass ../load-freetype.ps1  
+powershell -executionpolicy bypass ../load-libraries.ps1 %1
+powershell -executionpolicy bypass ./build.ps1 %1 
