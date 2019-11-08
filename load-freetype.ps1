c1b2daf5f095897d00d0d59b10d4acfda8a32b27
maxirmx <maxim@samsonov.net>
2019-11-08 18:27:02 +0300

    new file:   load-freetype.ps1

---
+++ load-freetype.ps1
@@ -0,0 +1,17 @@
+param (
+  [string] $version="2-10-1"
+)
+
+$fname  = "freetype2-VER-" + $version" 
+$fname1 =  $fname  + ".tar"
+$fname2 =  $fname1 + ".gz"
+
+
+Invoke-WebRequest -Uri "http://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/" + $fname2 -OutFile $fname2
+& "C:\Program Files\7-Zip\7z" -aoa x $fname2 
+& "C:\Program Files\7-Zip\7z" -aoa x $fname1 
+del $fname2
+del $fname1
+del pax_global_header
+ren $fname freetype
+
